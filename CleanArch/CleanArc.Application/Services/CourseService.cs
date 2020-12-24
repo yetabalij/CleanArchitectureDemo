980122406651133a016591a7303bf4b64a4f6016
Ermias <ermiastsegu@yahoo.com>
2020-12-24 13:31:48 +0300

    Sercvice implementation takes place

---
+++ CleanArch/CleanArc.Application/Services/CourseService.cs
@@ -0,0 +1,22 @@
+﻿using CleanArc.Application.Interfaces;
+using CleanArc.Application.ViewModels;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArc.Application.Services
+{
+    public class CourseService : ICourService
+    {
+        private ICourseRepository _courseRepository;
+        public CourseService(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+        public IEnumerable<CourseViewModel> GetCourses()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
